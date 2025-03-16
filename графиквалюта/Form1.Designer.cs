93dd8d38eba3e05612778a94ce1bfeb3a052d765
Александр <windowsalex484@gmail.com>
2025-03-16 22:53:23 +0300

    Добавьте файлы проекта.

---
+++ графиквалюта/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace графиквалюта
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea4.AxisX.MajorGrid.Enabled = false;
+            chartArea4.AxisY.MajorGrid.Enabled = false;
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(50, 140);
+            this.chart1.Name = "chart1";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(892, 328);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(245, 57);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(168, 24);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(474, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 28);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Обновить данные";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(632, 54);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 29);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Очистить данные";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1059, 575);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ графиквалюта/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+using System.Net.Http; // подключил библиотеку
+using Newtonsoft.Json.Linq; // подключил библиотеку
+
+namespace графиквалюта
+{
+    public partial class Form1 : Form
+    {
+        private JObject exchangeRates;
+        public Form1()
+        {
+            InitializeComponent();
+            LoadData(); 
+
+        }
+        private async void LoadData()
+        {
+            try
+            {
+                await GetExchangeRates();
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при загрузки данных: " + ex.Message);
+            }
+
+
+        }
+        public async Task GetExchangeRates()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    string url = "https://api.exchangerate-api.com/v4/latest/USD";
+                    string response = await client.GetStringAsync(url);
+                    JObject data = JObject.Parse(response);
+
+                    exchangeRates = (JObject)data["rates"];
+
+                    comboBox1.Items.Clear(); // очищаю список курсов валют
+                    JObject rates = (JObject)data["rates"]; // получаю список курсов валют
+
+                    foreach (var rate in exchangeRates)
+                    {
+                        comboBox1.Items.Add(rate.Key);
+                    }
+
+                    comboBox1.SelectedIndex = 0;
+
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка: " + ex.Message);
+                }
+
+
+            }
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            await GetExchangeRates();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (exchangeRates == null) return;
+
+            string selectedCurrency = comboBox1.SelectedItem.ToString(); // получаю валюту
+            double rate = exchangeRates[selectedCurrency].Value<double>(); // получаю курс
+
+            chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chart1.Series[0].Points.AddXY(DateTime.Now, rate); //
+
+            if (chart1.Series[0].Points.Count > 20)
+            {
+                chart1.Series[0].Points.RemoveAt(0);
+            }
+
+            chart1.ChartAreas[0].RecalculateAxesScale();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear(); // очищаю график
+        }
+    }
+}
